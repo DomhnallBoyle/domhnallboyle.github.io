59de13594c34ccc050a7801dc7326ffda1017955
Domhnall Boyle <domhnallboyle@gmail.com>
2024-12-06 16:27:52 +0000

    No redirecting

--- js/home.js
+++ js/home.js
@@ -1,4 +1,17 @@
+var links = document.querySelector('.list').children;
+links[0].style.color = 'lightblue';
+
+for (var i=0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', e => {
+        for (var j=0; j < links.length; j++)
+            links[j].style.color = 'gray';
+        e.target.style.color = 'lightblue';
+    });
+}
+
 document.querySelector('.branding').addEventListener('animationiteration', e => {
-    console.log("HERE");
-    window.location.href = "menu.html";
+    document.querySelector('.splashscreen').remove();
+
+    document.querySelector('.menu').style.display = 'block';
+    document.querySelector('.menu').style.visibility = 'visible';
 });
